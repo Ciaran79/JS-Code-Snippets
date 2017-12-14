855a426a4b4977c7f0506073234657b6ef153be0
Ciaran79 <ciarankamp@gmail.com>
2017-12-14 09:40:55 +0000

    latest

--- Kib conversion.js
+++ Kib conversion.js
@@ -2,14 +2,31 @@ function memorysizeConversion (memorysize){
 var newArray = memorysize.split(" ");
 var mult = 0;
 
-if (memorysize[1].indexOf("i")!=-1){
-    mult = 1.024;
+if (newArray[1].indexOf("i")!=-1){
+    mult =1.024 ;
 }
 else {
-    mult = 0.931;
+    mult = 0.976;
+}
+var result = "";
+
+switch (newArray[1]) {
+    case "kB":
+    result = "KiB";
+    break;
+    case "KiB":
+    result = "kB";
+    break;
+    case "GB":
+    result = "GiB";
+    break;
+    case "GiB":
+    result = "GB";
+    break;
 }
 
-  return memorysize[0] * mult;
+var num = newArray[0];
+  return (num * mult) + " " + result;
 }
 
-console.log(memorysizeConversion("2 kb"));
+console.log(memorysizeConversion("57.48 MiB"));
