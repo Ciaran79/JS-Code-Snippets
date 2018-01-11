ddbb05d07061d8de0ed66242be5810f57bed2ee2
Ciaran79 <ciarankamp@gmail.com>
2018-01-11 10:38:38 +0000

    latest

--- Flatten nested array.js
+++ Flatten nested array.js
@@ -1,15 +1,35 @@
 function steamrollArray(arr) {
+    var newArr =[];
    for (var i = 0; i < arr.length; i++) {
     if (Array.isArray(arr[i])){
-       if (arr[i].length<=1) {
-           arr.splice(charAt(arr[i][0]) , 1);
+        if(arr[i].length<1){
+            var blankArr = [];
+            newArr.push(blankArr);
+        
+        }
+        else {
+            arr[i] = arr[i].join();
+            arr[i] = arr[i].split(",");
+            var arrStr = arr[i];
+            for (let i = 0; i < arrStr.length; i++) {
+                if(typeof(arrStr[i])=== Number){
+                 arrStr[i] = Number(arrStr[i]);
+                }
+                
+            }
+            newArr = newArr.concat(arrStr);
+        }
+     
+      
     }
-}
+    else{
+        newArr.push(arr[i]);
+    }
    }
-    return arr;
+    return newArr;
   
 }
   
-  console.log(steamrollArray([1, [2], [3, [[4]]]]));
+  console.log(steamrollArray([1, [], [3, [[4]]]]));
   
   
